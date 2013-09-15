6478899bd64fbfeb3f001e8ed8e488bd87cd357f
Remorse <remorse@paddylee.com>
2013-09-15 01:23:04 -0400

    small fix to lattice_connect()

--- include/liblattice.h
+++ include/liblattice.h
@@ -3,4 +3,4 @@ extern int lattice_select(struct timeval *ptimeout);
 extern void lattice_process(void);
 extern int lattice_send(lattice_message *msg);
 extern void lattice_flush();
-extern int lattice_connect(n_coord coord, struct in_addr ip, port_t port);
+extern int lattice_connect(struct in_addr ip, port_t port);
