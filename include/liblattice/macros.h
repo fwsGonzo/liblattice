c6c0f8fa20e8919e4f9e12bca7046f5ea3b7dc9d
Remorse <remorse@paddylee.com>
2014-05-22 20:05:30 -0400

    some macros...

--- include/liblattice/macros.h
+++ include/liblattice/macros.h
@@ -32,4 +32,93 @@
   #define NOSLIDEOVER 0
   #define SLIDEOVER   1
 
+  #define get_uid get_uint32
+  #define put_uid put_uint32
+
+  #define get_model get_uint16
+  #define put_model put_uint16
+
+  #define get_color get_uint32
+  #define put_color put_uint32
+
+  #define get_nickname get_string
+  #define put_nickname put_string
+
+  #define get_chat get_string
+  #define put_chat put_string
+
+  #define get_action get_string
+  #define put_action put_string
+
+  #define get_quitreason get_string
+  #define put_quitreason put_string
+
+  #define get_burstdist get_uint16
+  #define put_burstdist put_uint16
+
+  #define get_block get_uint16
+  #define put_block put_uint16
+
+  #define get_wx get_int32
+  #define put_wx put_int32
+  #define get_wy get_int32
+  #define put_wy put_int32
+  #define get_wz get_int32
+  #define put_wz put_int32
+
+  #define get_bx get_int32
+  #define put_bx put_int32
+  #define get_by get_int32
+  #define put_by put_int32
+  #define get_bz get_int32
+  #define put_bz put_int32
+
+  #define get_nx get_int32
+  #define put_nx put_int32
+  #define get_ny get_int32
+  #define put_ny put_int32
+  #define get_nz get_int32
+  #define put_nz put_int32
+
+  #define get_xrot get_int16
+  #define get_yrot get_int16
+  #define put_xrot put_int16
+  #define put_yrot put_int16
+
+  #define get_item_id get_uint16
+  #define get_item_type get_uint16
+  #define put_item_id put_uint16
+  #define put_item_type put_uint16
+
+  #define get_mining get_uint16
+  #define put_mining put_uint16
+
+  #define get_usercolor get_uint32
+  #define put_usercolor put_uint32
+
+  #define get_satstep get_uint32
+  #define put_satstep put_uint32
+
+  #define get_sat get_double
+  #define put_sat put_double
+
+  #define get_mid get_int32
+  #define put_mid put_int32
+
+  #define get_sid get_int32
+  #define put_sid put_int32
+
+  #define get_csid get_int32
+  #define put_csid put_int32
+
+  #define get_boid get_int32
+  #define put_boid put_int32
+
+  #define get_moid get_int32
+  #define put_moid put_int32
+
+  #define get_mocount get_int32
+  #define put_mocount put_int32
+
+
 #endif
